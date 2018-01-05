8194d680ed485fdc13c43dc9cecba829ba8b514e
Liang <gaoxiaoliangz@users.noreply.github.com>
2018-01-05 17:05:41 +0800

    -

--- source/_src/index.js
+++ source/_src/index.js
@@ -1,10 +1,11 @@
 const kit = require('./kit')
 const mobilemenu = require('./mobile-menu')
 const contentplus = require('./content-plus')
+require('./funcs')
 
 hljs.initHighlightingOnLoad()
 
 kit.excAndExcOnResizing(function () {
-  contentplus.layoutContent(".content-plus");
-  mobilemenu.init();
-});
+  contentplus.layoutContent(".content-plus")
+  mobilemenu.init()
+})
